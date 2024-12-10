4144dd863f7a93fb7702b93f8e996297aac4fe10
obadaDeg <animayoloteer@gmail.com>
2024-12-10 15:38:33 +0200

    Collecting Form Data with states

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Custom Hooks
+# Forms
 
+## What's So Difficult About Forms?
+
+1. **Form Submission:**
+
+- Handling Submission is relatively **easy**.
+- Entered values can be managed via **state**.
+- Alternatively, they can be extracted via **Refs**.
+- Or via **FormData** and native browser features.
+
+2. **Input Validation:**
+
+- Providing a good user experience is **tricky**.
+- You can **validate** on every **keystroke** -> errors may be shown **too early.**
+- you can **validate** on **lost focus** -> errors may be shown **too long**.
+- You can **validate** on form **subbmision** -> errors may be shown **too late**.
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,61 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmission(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+
+  // somehting strange it adds undefined: undefined to the key values
+  // function handleInputChange({id, value}) {
+  //   setFormData((prevVals) => ({
+  //     ...prevVals,
+  //     [id]: value,
+  //   }));
+  // }
+
+  function handleInputChange(event) {
+    const { id, value } = event.target;
+
+    setFormData((prevVals) => ({
+      ...prevVals,
+      [id]: value,
+    }));
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmission}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            // onChange={(e) => {
+            //   handleInputChange(e.target);
+            // }} // somehting strange it adds undefined: undefined to the key values
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
         </div>
       </div>
 
