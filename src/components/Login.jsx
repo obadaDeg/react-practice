25ba0b9e576865f51ff46ca78f4bc834540a8277
obadaDeg <animayoloteer@gmail.com>
2024-12-11 09:35:38 +0200

    submission validation on Refs based forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,42 +1,53 @@
-import { useRef, useState } from "react";
+// coppied from his version from github but my idea was implemented on LoginStatful.jsx component
+ 
+
+import { useRef, useState } from 'react';
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
-  function handleSubmission(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    const emailEntred = email.current.value;
-    const passwordEntred = password.current.value;
 
-    console.log(emailEntred, passwordEntred);
-  }
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
 
-  function handleReset() {
-    email.current.value = "";
-    password.current.value = "";
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log('Sending HTTP request...');
   }
 
   return (
-    <form onSubmit={handleSubmission}>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input ref={email} id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input ref={password} id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat" onClick={handleReset}>
-          Reset
-        </button>
+        <button className="button button-flat">Reset</button>
         <button className="button">Login</button>
       </p>
     </form>
