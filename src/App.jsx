ee2444bce9530c91f9ee69072c107d16ee27eca0
obadaDeg <animayoloteer@gmail.com>
2024-12-12 15:55:25 +0200

    Router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,27 @@
-import { useDispatch, useSelector } from "react-redux";
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { useEffect } from "react";
-import { cartUIActions } from "./store/ui-slice";
-import { fetchCartData, sendCartData } from "./store/cart-actions";
+// Challenge / Exercise
 
-let isInitial = true;
+// 1. Add five new (dummy) page components (content can be simple <h1> elements)
+//    - HomePage
+//    - EventsPage
+//    - EventDetailPage
+//    - NewEventPage
+//    - EditEventPage
+// 2. Add routing & route definitions for these five pages
+//    - / => HomePage
+//    - /events => EventsPage
+//    - /events/<some-id> => EventDetailPage
+//    - /events/new => NewEventPage
+//    - /events/<some-id>/edit => EditEventPage
+// 3. Add a root layout that adds the <MainNavigation> component above all page components
+// 4. Add properly working links to the MainNavigation
+// 5. Ensure that the links in MainNavigation receive an "active" class when active
+// 6. Output a list of dummy events to the EventsPage
+//    Every list item should include a link to the respective EventDetailPage
+// 7. Output the ID of the selected event on the EventDetailPage
+// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 function App() {
-  const cartIsVisible = useSelector((state) => state.cartUI.cartIsVisible);
-  const cart = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchCartData());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (!isInitial) {
-      isInitial = false;
-      return;
-    }
-
-    if (cart.changed) {
-      dispatch(sendCartData(cart));
-    }
-  }, [dispatch, cart]);
-
-  return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
-  );
+  return <div></div>;
 }
 
 export default App;
